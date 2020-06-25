b348d72c3b08c8fee8fa870c301932a9ac0a0b88
Chris on Code <chris@scotch.io>
2020-06-25 16:52:58 -0700

    blahhh thanks for hanging in the stream

--- classroom.js
+++ classroom.js
@@ -1,1 +1,61 @@
-const Classroom = { template: '<div>{{ $route.params.classroomId }}</div>' };
+const Classroom = Vue.component('classroom', {
+  template: `
+    <div>
+      <h2 class="balsamiq text-4xl mb-12 text-white">You are in classroom #{{ $route.params.classroomId }}</h2>
+
+      <div class="text-gray-800 bg-white rounded-lg shadow-lg p-10">
+        <create-droplet-form class="mb-10" />
+        <droplets-list />
+      </div>
+    </div>
+  `,
+});
+
+/**
+ * Creation form to create droplet
+ */
+Vue.component('create-droplet-form', {
+  data: function () {
+    return {};
+  },
+  mounted: function () {},
+  methods: {
+    createDroplet: function () {},
+  },
+  template: `
+  <div class="relative bg-purple-200 rounded p-8">
+    
+    <h3 class="balsamiq text-2xl text-purple-900 mb-2">Create a Droplet</h3>
+    
+    <p class="mb-4 text-sm text-gray-700">This is where it all starts!</p>
+
+    <button @click="createDroplet" class="rounded text-sm py-3 px-10 bg-blue-500 shadow-lg text-blue-100 hover:bg-blue-400 hover:text-white hover:shadow-xl transition ease-in duration-100">Create a Droplet</button>
+
+    <img src="/space.svg" class="w-64 absolute right-0 top-0 mr-20" style="margin-top: -30px" />
+  </div>
+  `,
+});
+
+/**
+ * List all droplets
+ */
+Vue.component('droplets-list', {
+  data: function () {
+    return {
+      droplets: [
+        { id: 1, name: 'blah' },
+        { id: 1, name: 'blah' },
+        { id: 1, name: 'blah' },
+        { id: 1, name: 'blah' },
+      ],
+    };
+  },
+  // mounted: function () {
+  //   getDroplets();
+  // },
+  methods: {
+    getDroplets: function () {},
+    deleteDroplet: function (id) {},
+  },
+  template: `<div>list of droplets</div>`,
+});
--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const Home = Vue.component('home', {
   template: `
     <div>
-      <h2 class="text-4xl text-blue-100 mb-6">Join a Classroom</h2>
+      <h2 class="text-4xl text-white mb-6">Join a Classroom</h2>
     
       <join-class-form class="mb-10" />
       <classrooms-list />
@@ -17,7 +17,7 @@ Vue.component('join-class-form', {
   },
   methods: {
     joinClassroom: function () {
-      this.$router.push({ path: 'classroom', params: { classroomId: '123 ' } });
+      this.$router.push({ name: 'classroom', params: { classroomId: '123' } });
       // fetch('join-the-classroom.com')
       //   .then((res) => res.json())
       //   .then((data) => {
@@ -34,5 +34,37 @@ Vue.component('join-class-form', {
 });
 
 Vue.component('classrooms-list', {
-  template: `<div>blah</div>`,
+  data: function () {
+    return {
+      classrooms: [
+        { id: 2, name: 'math' },
+        { id: 2, name: 'science' },
+        { id: 2, name: 'chemistry' },
+        { id: 2, name: 'biology' },
+      ],
+    };
+  },
+  // mounted: function () {
+  //   getClassrooms();
+  // },
+  methods: {
+    //   getClassrooms: function () {
+    //     fetch('go-get-classrooms-for-a-user');
+    //   },
+    goToClassroom: function (id) {
+      this.$router.push({ name: 'classroom', params: { classroomId: id } });
+    },
+  },
+  template: `
+    <div>
+      <h2 class="balsamiq text-xl text-blue-200 mb-4">Classrooms <span class="text-sm opacity-75 ml-2">({{ classrooms.length }})</span></h2>
+
+      <div class="flex flex-wrap text-center">
+        <button class="w-24 h-24 mr-8 mb-8" v-for="(classroom, index) in classrooms" :key="index" @click="goToClassroom(classroom.id)">
+          <div class="icon w-full h-20 bg-yellow-400 mb-2 rounded-md hover:shadow-lg"></div>
+          <p class="text-xs text-blue-300">Random Text</p>
+        </button>
+      </div>
+    </div>
+  `,
 });
